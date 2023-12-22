ed5277336a1a23451593a07827938bed1130738a
suganya05 <101466647+suganya05@users.noreply.github.com>
2023-12-22 18:49:48 +0530

    fix:change content on pdf files and orders product files

--- src/hooks/postData.ts
+++ src/hooks/postData.ts
@@ -60,3 +60,68 @@ export const useGetPostData = ({ date }: { date?: Date }) => {
 
   return { loading, data: data };
 };
+
+export const usePostGetChart = ({ date }: { date?: Date }) => {
+  const [isDate, setIsDate] = useState<{ day: string; value: number }[]>([]);
+  console.log("isDate", isDate);
+
+  const handleGetData = useCallback(async () => {
+    try {
+      const today = date ? new Date(date) : new Date();
+      const startOfLastWeek = startOfDay(today);
+      startOfLastWeek.setDate(startOfLastWeek.getDate() - 6);
+
+      const daysData: { [key: string]: number[] } = {};
+
+      for (let i = 0; i < 7; i++) {
+        const currentDate = new Date(startOfLastWeek);
+        currentDate.setDate(currentDate.getDate() + i);
+        const dayOfWeek = currentDate.toLocaleDateString("en-US", {
+          weekday: "long",
+        });
+
+        daysData[dayOfWeek] = [];
+      }
+
+      const endOfToday = endOfDay(today);
+      const Mid = collection(db, ORDERS_COLLECTION_NAME);
+      const midProducts = query(
+        Mid,
+        where("type", "==", "PostLevel"),
+        where("createdAt", ">=", startOfLastWeek),
+        where("createdAt", "<=", endOfToday),
+        orderBy("createdAt", "asc")
+        // where("paymentStatus", "==", "SUCCESS")
+      );
+      const midData = await getDocs(midProducts);
+
+      midData.forEach((doc) => {
+        const midPostdata = doc.data();
+        const createdAt = midPostdata.createdAt.toDate();
+        const dayOfWeek = createdAt.toLocaleDateString("en-US", {
+          weekday: "long",
+        });
+
+        if (createdAt >= startOfLastWeek && createdAt <= endOfToday) {
+          const { price } = midPostdata;
+          daysData[dayOfWeek].push(Number(price));
+        }
+      });
+
+      const totalPerDay = Object.entries(daysData).map(([day, dayData]) => ({
+        day,
+        value: dayData.reduce((acc, curr) => acc + curr, 0),
+      }));
+
+      setIsDate(totalPerDay);
+    } catch (error) {
+      console.log(error);
+    }
+  }, [date]);
+
+  useEffect(() => {
+    handleGetData();
+  }, [handleGetData, date]);
+
+  return { data: isDate };
+};
